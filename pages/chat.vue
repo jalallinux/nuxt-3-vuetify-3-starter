c77f9550bd753871f9baaa7cad2325f670a7b6ca
jalallinux <smjjalalzadeh93@gmail.com>
2023-04-23 21:07:31 +0330

    work on navbar

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,25 +1,21 @@
 <template>
   <v-app id="chat">
-<!--    <v-system-bar>-->
-<!--      <v-spacer />-->
-<!--      <v-icon>mdi-square</v-icon>-->
-<!--      <v-icon>mdi-circle</v-icon>-->
-<!--      <v-icon>mdi-triangle</v-icon>-->
-<!--    </v-system-bar>-->
 
-    <v-navigation-drawer absolute color="grey-lighten-3" rail>
-      <v-avatar class="d-block text-center mx-auto mt-4" color="grey-lighten-1" size="48" image="https://avatars.githubusercontent.com/u/37062636?v=4"/>
-      <v-divider class="mx-3 my-4" />
-      <v-btn v-for="user in data.users" :key="user.id" :to="`#${user.username}`" :active="useRoute().hash === `#${user.username}`"
-             size="42" icon class="d-block text-center mx-auto mb-5 overflow-hidden rounded-circle bg-transparent">
-        <v-img :src="user.image" />
-      </v-btn>
-    </v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer" :rail="rail" permanent absolute @click="rail = false" color="grey-lighten-3">
+      <v-list-item class="my-4" nav :prepend-avatar="me.avatar_url" :title="me.name" :subtitle="`From ${me.location}`">
+        <template v-slot:append>
+          <v-btn variant="plain" icon="mdi-chevron-left" @click.stop="rail = !rail" />
+        </template>
+      </v-list-item>
 
-    <v-navigation-drawer width="244">
-      <v-sheet color="grey-lighten-5" height="128" width="100%"/>
-      <v-list>
-        <v-list-item v-for="n in 5" :key="n" :title="`Item ${ n }`" link>
+      <v-divider />
+
+      <v-list density="compact" class="pa-0" nav>
+        <v-list-item v-for="user in data.users" :key="user.id" :to="`#${user.username}`" :active="useRoute().hash === `#${user.username}`"
+                     :title="`${user.firstName} ${user.lastName}`" :value="user.username">
+          <template v-slot:prepend>
+            <v-avatar size="42" :image="user.image" />
+          </template>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -47,8 +43,11 @@
 </template>
 
 <script setup>
-const { data: me } = await useFetch("https://dummyjson.com/users/100")
+const { data: me } = await useFetch("https://api.github.com/users/jalallinux")
 const { data } = await useFetch("https://dummyjson.com/users", { query: { limit: 8 } })
+
+const drawer = ref(true)
+const rail = ref(true)
 
 </script>
 
